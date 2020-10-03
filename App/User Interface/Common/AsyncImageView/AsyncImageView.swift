40477ca8a8db218d9bc0ec4c5bcb53e7698fb32b
Fernando Bonfim <fbonfim1@its.jnj.com>
2020-10-03 02:30:31 -0300

    Adding icon, round corner and shadows

--- App/User Interface/Common/AsyncImageView/AsyncImageView.swift
+++ App/User Interface/Common/AsyncImageView/AsyncImageView.swift
@@ -9,11 +9,32 @@ import Core
 import PromiseKit
 import UIKit
 
-class AsyncImageView: UIImageView {
+class AsyncImageView: UIView {
     var viewModel = AsyncImageViewModel()
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.layer.cornerRadius = 10.0
+        view.clipsToBounds = true
+        return view
+    }()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = .init(width: 3.0, height: 3.0)
+        layer.shadowRadius = 3.0
+        layer.shadowOpacity = 0.5
+        clipsToBounds = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
 
     func setPlaceholder()  {
-        image = viewModel.placeholderImage
+        imageView.image = viewModel.placeholderImage
         showLoading()
     }
 
@@ -23,7 +44,7 @@ class AsyncImageView: UIImageView {
             .loadImage(url)
             .done { [weak self] image in
                 self?.hideLoading()
-                self?.image = image
+                self?.imageView.image = image
             }.cauterize()
     }
 }
